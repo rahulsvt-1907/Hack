a0588ddd5fb3a33c06449e2d3475813b9c1115fd
Rahul Srivastava <rahulgsrivastava1906@gmail.com>
2026-03-18 23:37:09 +0530

    adding comments

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -9,10 +9,30 @@ model = ChatGroq(model="groq/compound-mini")
 #to invoke model we use model.invoke method
 #and then we should save that response
 
-response = model.invoke("What is Rag")
+response = model.invoke("Hello")
 
 
 # print(response) we have to only take content from the answer
 
 print(response.content)
+
+
+
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# from langchain_groq import ChatGroq
+
+# model = ChatGroq(model="groq/compound-mini")
+
+# #to invoke model we use model.invoke method
+# #and then we should save that response
+
+# response = model.invoke("What is Rag")
+
+
+# # print(response) we have to only take content from the answer
+
+# print(response.content)
 
